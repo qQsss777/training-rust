4ba12219e4ab19b533ab02e0d291ab0f535e39d2
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2026-04-13 18:01:10 +0200

    thread

---
+++ README.md
@@ -0,0 +1,4 @@
+## thread
+- cargo run -p thread
+- cargo run -p thread attach
+- cargo run -p thread multi
--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,9 +1,10 @@
+use std::env;
 use std::{
-    thread::{self, sleep},
+    thread::{self},
     time::Duration,
 };
 
-fn main() {
+fn multi_thread() {
     loop {
         for i in 1..8 {
             let _ = thread::Builder::new().name(i.to_string()).spawn(move || {
@@ -17,5 +18,43 @@ fn main() {
             println!("hi number {i} from the main thread!");
             thread::sleep(Duration::from_secs(10));
         }
+    }
+}
+
+fn default() {
+    let handler = thread::spawn(move || {
+        println!("Hello from other thread");
+    });
+    println!("Hello from main thread");
+    handler.join().unwrap();
+    println!("Hello from main thread second time");
+}
+
+fn attach_thread() {
+    thread::spawn(move || {
+        println!("Hello from other thread");
+    });
+    println!("Hello from main thread");
+    println!("Hello from main thread");
+    println!("Hello from main thread");
+
+    // si pas de sleep, le programme se termine car le thread est détaché
+    thread::sleep(Duration::from_secs(3));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => {
+            let value: &str = args[1].as_str();
+            match value {
+                "multi" => multi_thread(),
+                "attach" => attach_thread(),
+                _ => {
+                    default();
+                }
+            };
+        }
+        _ => default(),
     }
 }
